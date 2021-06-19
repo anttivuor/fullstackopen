d3932c3ed198953933c5f45cd6ed74cc794c0aa5
Antti Vuorenmaa <antti@vertics.co>
2021-06-19 18:45:52 +0300

    Exercises 4.1-4.2

---
+++ osa4/blogilista/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+
+const blogRouter = require('./controllers/blog')
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogRouter)
+
+const PORT = config.PORT
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`)
+})
